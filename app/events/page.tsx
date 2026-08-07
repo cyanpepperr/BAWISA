86a815c079ba94fd12cdc4b8e9aaa43af2ff689f
HP-LAPTOP-01\Coral14 <cy_yee@berkeley.edu>
2026-08-06 22:18:05 -0700

    magenta test events

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -52,9 +52,7 @@ export default function EventsPage() {
   return (
     <div className="relative isolate">
       {/* Page-wide starfield background */}
-      <div className="fixed inset-0 -z-10">
-        <StarfieldBackground />
-      </div>
+      <div className="fixed inset-0 -z-10" style={{ backgroundColor: 'magenta' }} />
 
       <PageHero
         transparent
