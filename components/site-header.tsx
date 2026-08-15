8f0ed4a68160a12bb53e467fa8b5c372d7893641
chloeplum <chloe_stevens@berkeley.edu>
2026-08-14 20:01:55 -0700

    Update site-header.tsx

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -22,7 +22,7 @@ export function SiteHeader() {
           onClick={() => setOpen(false)}
         >
           <Image
-            src="/white-logo.png"
+            src="/amethyst-smoke-logo.png"
             alt="BAWISA logo"
             width={45}
             height={45}
